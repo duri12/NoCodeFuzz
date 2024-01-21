7478dc29f153cf783f9b7bbc02564757e6919618
duri12 <eyalgor102@gmail.com>
2024-01-21 18:33:58 +0200

    added busy wait

--- honggfuzz/subproc.c
+++ honggfuzz/subproc.c
@@ -47,9 +47,9 @@
 #include "libhfcommon/log.h"
 #include "libhfcommon/util.h"
 
-extern char** environ;
+extern char **environ;
 
-const char* subproc_StatusToStr(int status) {
+const char *subproc_StatusToStr(int status) {
     static __thread char str[256];
 
     if (WIFEXITED(status)) {
@@ -59,7 +59,7 @@ const char* subproc_StatusToStr(int status) {
 
     if (WIFSIGNALED(status)) {
         snprintf(str, sizeof(str), "SIGNALED, signal: %d (%s)", WTERMSIG(status),
-            strsignal(WTERMSIG(status)));
+                 strsignal(WTERMSIG(status)));
         return str;
     }
     if (WIFCONTINUED(status)) {
@@ -75,7 +75,7 @@ const char* subproc_StatusToStr(int status) {
     /* Must be in a stopped state */
     if (WSTOPSIG(status) == (SIGTRAP | 0x80)) {
         snprintf(str, sizeof(str), "STOPPED (linux syscall): %d (%s)", WSTOPSIG(status),
-            strsignal(WSTOPSIG(status)));
+                 strsignal(WSTOPSIG(status)));
         return str;
     }
 #if defined(PTRACE_EVENT_STOP)
@@ -123,90 +123,93 @@ const char* subproc_StatusToStr(int status) {
 #endif /*  defined(PTRACE_EVENT_STOP)  */
 
     snprintf(str, sizeof(str), "STOPPED with signal: %d (%s)", WSTOPSIG(status),
-        strsignal(WSTOPSIG(status)));
+             strsignal(WSTOPSIG(status)));
     return str;
 }
 
-static bool subproc_persistentSendFileIndicator(run_t* run) {
-    uint64_t len = (uint64_t)run->dynfile->size;
-    if (!files_sendToSocketNB(run->persistentSock, (uint8_t*)&len, sizeof(len))) {
+static bool subproc_persistentSendFileIndicator(run_t *run) {
+    uint64_t len = (uint64_t) run->dynfile->size;
+    if (!files_sendToSocketNB(run->persistentSock, (uint8_t * ) & len, sizeof(len))) {
         PLOG_W("files_sendToSocketNB(len=%zu)", sizeof(len));
         return false;
     }
     return true;
 }
 
-static bool subproc_persistentGetReady(run_t* run) {
+static bool subproc_persistentGetReady(run_t *run) {
     uint8_t rcv;
     if (recv(run->persistentSock, &rcv, sizeof(rcv), MSG_DONTWAIT) != sizeof(rcv)) {
         return false;
     }
     if (rcv != HFReadyTag) {
         LOG_E("Received invalid message from the persistent process: '%c' (0x%" PRIx8
-              ") , expected '%c' (0x%" PRIx8 ")",
-            rcv, rcv, HFReadyTag, HFReadyTag);
+                      ") , expected '%c' (0x%" PRIx8 ")",
+              rcv, rcv, HFReadyTag, HFReadyTag);
         return false;
     }
     return true;
 }
 
-bool subproc_persistentModeStateMachine(run_t* run) {
+bool subproc_persistentModeStateMachine(run_t *run) {
     if (!run->global->exe.persistent) {
         return false;
     }
 
     for (;;) {
         switch (run->runState) {
-        case _HF_RS_WAITING_FOR_INITIAL_READY: {
-            if (!subproc_persistentGetReady(run)) {
-                return false;
-            }
-            run->runState = _HF_RS_SEND_DATA;
-        }; break;
-        case _HF_RS_SEND_DATA: {
-            if (!subproc_persistentSendFileIndicator(run)) {
-                LOG_E("Could not send the file size indicator to the persistent process. "
-                      "Killing the process pid=%d",
-                    (int)run->pid);
-                kill(run->pid, SIGKILL);
-                return false;
-            }
-            run->runState = _HF_RS_WAITING_FOR_READY;
-        }; break;
-        case _HF_RS_WAITING_FOR_READY: {
-            if (!subproc_persistentGetReady(run)) {
-                return false;
-            }
-            run->runState = _HF_RS_SEND_DATA;
-            /* The current persistent round is done */
-            return true;
-        }; break;
-        default:
+            case _HF_RS_WAITING_FOR_INITIAL_READY: {
+                if (!subproc_persistentGetReady(run)) {
+                    return false;
+                }
+                run->runState = _HF_RS_SEND_DATA;
+            };
+                break;
+            case _HF_RS_SEND_DATA: {
+                if (!subproc_persistentSendFileIndicator(run)) {
+                    LOG_E("Could not send the file size indicator to the persistent process. "
+                          "Killing the process pid=%d",
+                          (int) run->pid);
+                    kill(run->pid, SIGKILL);
+                    return false;
+                }
+                run->runState = _HF_RS_WAITING_FOR_READY;
+            };
+                break;
+            case _HF_RS_WAITING_FOR_READY: {
+                if (!subproc_persistentGetReady(run)) {
+                    return false;
+                }
+                run->runState = _HF_RS_SEND_DATA;
+                /* The current persistent round is done */
+                return true;
+            };
+                break;
+            default:
             LOG_F("Unknown runState: %d", run->runState);
         }
     }
 }
 
-static void subproc_prepareExecvArgs(run_t* run) {
+static void subproc_prepareExecvArgs(run_t *run) {
     size_t x = 0;
-    for (x = 0; x < _HF_ARGS_MAX && x < (size_t)run->global->exe.argc; x++) {
-        const char* ph_str = strstr(run->global->exe.cmdline[x], _HF_FILE_PLACEHOLDER);
+    for (x = 0; x < _HF_ARGS_MAX && x < (size_t) run->global->exe.argc; x++) {
+        const char *ph_str = strstr(run->global->exe.cmdline[x], _HF_FILE_PLACEHOLDER);
         if (!strcmp(run->global->exe.cmdline[x], _HF_FILE_PLACEHOLDER)) {
             run->args[x] = _HF_INPUT_FILE_PATH;
         } else if (ph_str) {
             static __thread char argData[PATH_MAX];
             snprintf(argData, sizeof(argData), "%.*s%s",
-                (int)(ph_str - run->global->exe.cmdline[x]), run->global->exe.cmdline[x],
-                _HF_INPUT_FILE_PATH);
+                     (int) (ph_str - run->global->exe.cmdline[x]), run->global->exe.cmdline[x],
+                     _HF_INPUT_FILE_PATH);
             run->args[x] = argData;
         } else {
-            run->args[x] = (char*)run->global->exe.cmdline[x];
+            run->args[x] = (char *) run->global->exe.cmdline[x];
         }
     }
     run->args[x] = NULL;
 }
 
-static bool subproc_PrepareExecv(run_t* run) {
+static bool subproc_PrepareExecv(run_t *run) {
     util_ParentDeathSigIfAvail(SIGKILL);
 
     /*
@@ -274,7 +277,8 @@ static bool subproc_PrepareExecv(run_t* run) {
         putenv(run->global->exe.env_ptrs[i]);
     }
     char fuzzNo[128];
-    snprintf(fuzzNo, sizeof(fuzzNo), "%" PRId32, run->fuzzNo);
+    snprintf(fuzzNo, sizeof(fuzzNo), "%"
+    PRId32, run->fuzzNo);
     setenv(_HF_THREAD_NO_ENV, fuzzNo, 1);
     if (run->global->exe.netDriver) {
         setenv(_HF_THREAD_NETDRIVER_ENV, "1", 1);
@@ -284,27 +288,27 @@ static bool subproc_PrepareExecv(run_t* run) {
     setsid();
 
     util_closeStdio(/* close_stdin= */ run->global->exe.nullifyStdio,
-        /* close_stdout= */ run->global->exe.nullifyStdio,
-        /* close_stderr= */ run->global->exe.nullifyStdio);
+            /* close_stdout= */ run->global->exe.nullifyStdio,
+            /* close_stderr= */ run->global->exe.nullifyStdio);
 
     /* The coverage bitmap/feedback structure */
     if (TEMP_FAILURE_RETRY(dup2(run->global->feedback.covFeedbackFd, _HF_COV_BITMAP_FD)) == -1) {
         PLOG_E("dup2(%d, _HF_COV_BITMAP_FD=%d)", run->global->feedback.covFeedbackFd,
-            _HF_COV_BITMAP_FD);
+               _HF_COV_BITMAP_FD);
         return false;
     }
     /* The const comparison bitmap/feedback structure */
     if (run->global->feedback.cmpFeedback &&
         TEMP_FAILURE_RETRY(dup2(run->global->feedback.cmpFeedbackFd, _HF_CMP_BITMAP_FD)) == -1) {
         PLOG_E("dup2(%d, _HF_CMP_BITMAP_FD=%d)", run->global->feedback.cmpFeedbackFd,
-            _HF_CMP_BITMAP_FD);
+               _HF_CMP_BITMAP_FD);
         return false;
     }
 
     /* The per-thread coverage feedback bitmap */
     if (TEMP_FAILURE_RETRY(dup2(run->perThreadCovFeedbackFd, _HF_PERTHREAD_BITMAP_FD)) == -1) {
         PLOG_E("dup2(%d, _HF_CMP_PERTHREAD_FD=%d)", run->perThreadCovFeedbackFd,
-            _HF_PERTHREAD_BITMAP_FD);
+               _HF_PERTHREAD_BITMAP_FD);
         return false;
     }
 
@@ -315,7 +319,7 @@ static bool subproc_PrepareExecv(run_t* run) {
             PLOG_E("dup2('%d', _HF_INPUT_FD='%d')", run->dynfile->fd, _HF_INPUT_FD);
             return false;
         }
-        if (lseek(_HF_INPUT_FD, 0, SEEK_SET) == (off_t)-1) {
+        if (lseek(_HF_INPUT_FD, 0, SEEK_SET) == (off_t) - 1) {
             PLOG_E("lseek(_HF_INPUT_FD=%d, 0, SEEK_SET)", _HF_INPUT_FD);
             return false;
         }
@@ -348,7 +352,7 @@ static bool subproc_PrepareExecv(run_t* run) {
 }
 
 
-static bool subproc_New(run_t* run) {
+static bool subproc_New(run_t *run) {
     if (run->pid) {
         return true;
     }
@@ -421,7 +425,7 @@ static bool subproc_New(run_t* run) {
     }
 
     /* Parent */
-    LOG_D("Launched new process, pid=%d, thread: %" PRId32 " (concurrency: %zd)", (int)run->pid,
+    LOG_D("Launched new process, pid=%d, thread: %" PRId32 " (concurrency: %zd)", (int) run->pid,
           run->fuzzNo, run->global->threads.threadsMax);
 
     arch_prepareParentAfterFork(run);
@@ -429,20 +433,19 @@ static bool subproc_New(run_t* run) {
     if (run->global->exe.persistent) {
         close(sv[1]);
         run->runState = _HF_RS_WAITING_FOR_INITIAL_READY;
-        LOG_I("Persistent mode: Launched new persistent pid=%d", (int)run->pid);
+        LOG_I("Persistent mode: Launched new persistent pid=%d", (int) run->pid);
     }
 
     return true;
 }
 
 
-static void MyFunction(char * password){
+static void MyFunction(char *password) {
 
-    if(password[6] != '\0')
+    if (password[6] != '\0')
         return;
 
-    if(password[0]=='P')
-    {
+    if (password[0] == 'P') {
         __asm__ __volatile__
         (
         "pause;"
@@ -454,10 +457,9 @@ static void MyFunction(char * password){
         "jg loop0;"
         :
         :
-        : "rdx","rbx","cc", "memory"
+        : "rdx", "rbx", "cc", "memory"
         );
-        if(password[1]=='A')
-        {
+        if (password[1] == 'A') {
             __asm__ __volatile__
             (
             "pause;"
@@ -469,10 +471,9 @@ static void MyFunction(char * password){
             "jg loop1;"
             :
             :
-            : "rdx","rbx","cc", "memory"
+            : "rdx", "rbx", "cc", "memory"
             );
-            if(password[2]=='S')
-            {
+            if (password[2] == 'S') {
                 __asm__ __volatile__
                 (
                 "pause;"
@@ -484,10 +485,9 @@ static void MyFunction(char * password){
                 "jg loop2;"
                 :
                 :
-                : "rdx","rbx","cc", "memory"
+                : "rdx", "rbx", "cc", "memory"
                 );
-                if(password[3]=='s')
-                {
+                if (password[3] == 's') {
                     __asm__ __volatile__
                     (
                     "pause;"
@@ -499,10 +499,9 @@ static void MyFunction(char * password){
                     "jg loop3;"
                     :
                     :
-                    : "rdx","rbx","cc", "memory"
+                    : "rdx", "rbx", "cc", "memory"
                     );
-                    if(password[4]=='1')
-                    {
+                    if (password[4] == '1') {
                         __asm__ __volatile__
                         (
                         "pause;"
@@ -514,10 +513,9 @@ static void MyFunction(char * password){
                         "jg loop5;"
                         :
                         :
-                        : "rdx","rbx","cc", "memory"
+                        : "rdx", "rbx", "cc", "memory"
                         );
-                        if(password[5]=='!')
-                        {
+                        if (password[5] == '!') {
                             __asm__ __volatile__
                             (
                             "pause;"
@@ -529,7 +527,7 @@ static void MyFunction(char * password){
                             "jg loop6;"
                             :
                             :
-                            : "rdx","rbx","cc", "memory"
+                            : "rdx", "rbx", "cc", "memory"
                             );
                             LOG_I("found it !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
                             sleep(10);
@@ -541,7 +539,7 @@ static void MyFunction(char * password){
     }
 }
 
-static bool subproc_runNoFork(run_t* run) {
+static bool subproc_runNoFork(run_t *run) {
     if (run->global->exe.persistent)
         subproc_New(run); /*should not run . here to skip unused error*/
     /*set up the environment*/
@@ -554,7 +552,7 @@ static bool subproc_runNoFork(run_t* run) {
     }
 
 
-    subproc_prepareExecvArgs(run) ;/* put the args in run->args*/
+    subproc_prepareExecvArgs(run);/* put the args in run->args*/
 
     /*
     * Disable ASLR:
@@ -573,31 +571,31 @@ static bool subproc_runNoFork(run_t* run) {
 
     char password[1024];
 
-    strncpy(password,(char * )run->dynfile->data, 8);
+    strncpy(password, (char *) run->dynfile->data, 8);
 
     // if(strlen(password) != 6)
     //     return 0;
 
     __asm__ __volatile__ ("CPUID\n\t"
-                  "RDTSC\n\t"
-                  "mov %%edx, %0\n\t"                                                                                 "mov %%eax, %1\n\t": "=r" (cycles_high_start), "=r" (cycles_low_start)::
+                          "RDTSC\n\t"
+                          "mov %%edx, %0\n\t"                                                                                 "mov %%eax, %1\n\t": "=r" (cycles_high_start), "=r" (cycles_low_start)::
     "%rax", "%rbx", "%rcx", "%rdx");
 
     MyFunction(password);
 
     __asm__ __volatile__("RDTSCP\n\t"
-                 "mov %%edx, %0\n\t"
-                 "mov %%eax, %1\n\t"
-                 "CPUID\n\t": "=r" (cycles_high_end), "=r" (cycles_low_end)::
+                         "mov %%edx, %0\n\t"
+                         "mov %%eax, %1\n\t"
+                         "CPUID\n\t": "=r" (cycles_high_end), "=r" (cycles_low_end)::
     "%rax", "%rbx", "%rcx", "%rdx");
 
     //interprets values
-    start = ( ((uint64_t)cycles_high_start << 32) | cycles_low_start );
-    end = ( ((uint64_t)cycles_high_end << 32) | cycles_low_end);
+    start = (((uint64_t) cycles_high_start << 32) | cycles_low_start);
+    end = (((uint64_t) cycles_high_end << 32) | cycles_low_end);
 
-    int64_t instrCount = end-start;
-    if (run->global->feedback.dynFileMethod & _HF_DYNFILE_INSTR_COUNT){
-        run->hwCnts.cpuInstrCnt  = instrCount;
+    int64_t instrCount = end - start;
+    if (run->global->feedback.dynFileMethod & _HF_DYNFILE_INSTR_COUNT) {
+        run->hwCnts.cpuInstrCnt = instrCount;
     }
 
     int64_t diffUSecs = util_timeNowUSecs() - run->timeStartedUSecs;
@@ -613,7 +611,7 @@ static bool subproc_runNoFork(run_t* run) {
 }
 
 
-bool subproc_Run(run_t* run) {
+bool subproc_Run(run_t *run) {
     /*
     if (!subproc_New(run)) {
         LOG_E("subproc_New()");
@@ -637,7 +635,7 @@ bool subproc_Run(run_t* run) {
     return subproc_runNoFork(run);
 }
 
-uint8_t subproc_System(run_t* run, const char* const argv[]) {
+uint8_t subproc_System(run_t *run, const char *const argv[]) {
     pid_t pid = arch_fork(run);
     if (pid == -1) {
         PLOG_E("Couldn't fork");
@@ -648,7 +646,7 @@ uint8_t subproc_System(run_t* run, const char* const argv[]) {
 
         setsid();
         util_closeStdio(
-            /* close_stdin= */ true, /* close_stdout= */ false, /* close_stderr= */ false);
+                /* close_stdin= */ true, /* close_stdout= */ false, /* close_stderr= */ false);
 
         sigset_t sset;
         sigemptyset(&sset);
@@ -656,7 +654,7 @@ uint8_t subproc_System(run_t* run, const char* const argv[]) {
             PLOG_W("sigprocmask(empty_set)");
         }
 
-        execv(argv[0], (char* const*)&argv[0]);
+        execv(argv[0], (char *const *) &argv[0]);
         PLOG_F("Couldn't execute '%s'", argv[0]);
         return 255;
     }
@@ -673,11 +671,11 @@ uint8_t subproc_System(run_t* run, const char* const argv[]) {
         int status;
         int ret = TEMP_FAILURE_RETRY(wait4(pid, &status, flags, NULL));
         if (ret == -1) {
-            PLOG_E("wait4() for process pid=%d", (int)pid);
+            PLOG_E("wait4() for process pid=%d", (int) pid);
             return 255;
         }
         if (ret != pid) {
-            LOG_E("wait4() returned %d, but waited for %d", ret, (int)pid);
+            LOG_E("wait4() returned %d, but waited for %d", ret, (int) pid);
             return 255;
         }
         if (WIFSIGNALED(status)) {
@@ -696,27 +694,27 @@ uint8_t subproc_System(run_t* run, const char* const argv[]) {
     }
 }
 
-void subproc_checkTimeLimit(run_t* run) {
+void subproc_checkTimeLimit(run_t *run) {
     if (!run->global->timing.tmOut) {
         return;
     }
 
-    int64_t curUSecs  = util_timeNowUSecs();
+    int64_t curUSecs = util_timeNowUSecs();
     int64_t diffUSecs = curUSecs - run->timeStartedUSecs;
 
     if (run->tmOutSignaled && (diffUSecs > ((run->global->timing.tmOut + 1) * 1000000))) {
         /* Has this instance been already signaled due to timeout? Just, SIGKILL it */
         LOG_W("pid=%d has already been signaled due to timeout. Killing it with SIGKILL",
-            (int)run->pid);
+              (int) run->pid);
         kill(run->pid, SIGKILL);
         return;
     }
 
     if ((diffUSecs > (run->global->timing.tmOut * 1000000)) && !run->tmOutSignaled) {
         run->tmOutSignaled = true;
-        LOG_W("pid=%d took too much time (limit %ld s). Killing it with %s", (int)run->pid,
-            (long)run->global->timing.tmOut,
-            run->global->timing.tmoutVTALRM ? "SIGVTALRM" : "SIGKILL");
+        LOG_W("pid=%d took too much time (limit %ld s). Killing it with %s", (int) run->pid,
+              (long) run->global->timing.tmOut,
+              run->global->timing.tmoutVTALRM ? "SIGVTALRM" : "SIGKILL");
         if (run->global->timing.tmoutVTALRM) {
             kill(run->pid, SIGVTALRM);
         } else {
@@ -726,24 +724,24 @@ void subproc_checkTimeLimit(run_t* run) {
     }
 }
 
-void subproc_checkTermination(run_t* run) {
+void subproc_checkTermination(run_t *run) {
     if (fuzz_isTerminating()) {
-        LOG_D("Killing pid=%d", (int)run->pid);
+        LOG_D("Killing pid=%d", (int) run->pid);
         kill(run->pid, SIGKILL);
     }
 }
 
 bool subproc_runThread(
-    honggfuzz_t* hfuzz, pthread_t* thread, void* (*thread_func)(void*), bool joinable) {
+        honggfuzz_t *hfuzz, pthread_t *thread, void *(*thread_func)(void *), bool joinable) {
     pthread_attr_t attr;
 
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(
-        &attr, joinable ? PTHREAD_CREATE_JOINABLE : PTHREAD_CREATE_DETACHED);
+            &attr, joinable ? PTHREAD_CREATE_JOINABLE : PTHREAD_CREATE_DETACHED);
     pthread_attr_setstacksize(&attr, _HF_PTHREAD_STACKSIZE);
-    pthread_attr_setguardsize(&attr, (size_t)sysconf(_SC_PAGESIZE));
+    pthread_attr_setguardsize(&attr, (size_t) sysconf(_SC_PAGESIZE));
 
-    if (pthread_create(thread, &attr, thread_func, (void*)hfuzz) < 0) {
+    if (pthread_create(thread, &attr, thread_func, (void *) hfuzz) < 0) {
         PLOG_W("Couldn't create a new thread");
         return false;
     }
