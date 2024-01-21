d22f7ce2fc325e09eddc410da7568eb4f3071c27
duri12 <eyalgor102@gmail.com>
2024-01-21 19:17:04 +0200

    reverted

--- honggfuzz/subproc.c
+++ honggfuzz/subproc.c
@@ -452,10 +452,7 @@ static void MyFunction(char *password) {
         "pause;"
         "movq $0, %%rdx;"
         "movq $0x10000, %%rbx;"
-#ifndef LOOP0
-#define LOOP0
         "loop0:"
-#endif
         "inc %%rdx;"
         "cmpq %%rdx, %%rbx;"
         "jg loop0;"
