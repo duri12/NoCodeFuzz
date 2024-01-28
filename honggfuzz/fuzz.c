66c549768da0229e4135e3bf4e7c99c0a06990b8
Ofir Shapira <48770263+sharp30@users.noreply.github.com>
2024-01-28 21:43:30 +0200

    updated threshold

--- honggfuzz/fuzz.c
+++ honggfuzz/fuzz.c
@@ -249,7 +249,7 @@ static void fuzz_perfFeedback(run_t* run) {
     int64_t diff_instrCnt = (int64_t)run->global->feedback.hwCnts.cpuInstrCnt - run->hwCnts.cpuInstrCnt;
     int64_t diff_cpuBranchCnt = (int64_t)run->global->feedback.hwCnts.cpuBranchCnt - run->hwCnts.cpuBranchCnt;
 
-    int threshold = 20;
+    int threshold = 60;
     /* Any increase in coverage (edge, pc, cmp, hw) counters forces adding input to the corpus */
     if((distance > threshold) ||((distance==-1) && (run->hwCnts.newBBCnt > 0 || softNewPC > 0 || softNewEdge > 0 || softNewCmp > 0 ||
         diff_instrCnt < 0 || diff_cpuBranchCnt < 0)))
