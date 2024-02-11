e9ec606e00d1ef869358e5363527588a63dbd647
duri12 <eyalgor102@gmail.com>
2024-02-11 19:03:38 +0200

    added pht_release,removed duplicate code  and fix some comments

--- honggfuzz/side-channels/pht_PP_api.c
+++ honggfuzz/side-channels/pht_PP_api.c
@@ -12,41 +12,36 @@
 #define FUNC_SIZE 17
 
 
-//NOTE: take it from util.h - we should include all our uses from the same place, I added code in comment in the h file.
-/*uint64_t rdtsc() {
-    // Utility functions from https://github.com/IAIK/transientfail/ /
-    uint64_t a, d;
-    asm volatile("mfence");
-    asm volatile("rdtscp" : "=a"(a), "=d"(d)::"rcx");
-    a = (d << 32) | a;
-    asm volatile("mfence");
-    return a;
-}*/
-
-//TODO: can you please put the asm code you implemented here in comment?
 //NOTE: if you could make the length to be 16 instead of 17 - could help a lot
 //BTW - why not using loop? or memcpy? looks pretty ugly
-//BTW: you could just sent p+i as p.
+//BTW: you could just send p+i as p.
 void write_probe(uint8_t* p,int i){
-    p[i] =0x55;
-    p[i+1] =0x48;
+    p[i] =0x55;  // push   rbp
+    p[i+1] =0x48;// mov    rbp,rsp
     p[i+2] =0x89;
     p[i+3] =0xe5;
-    p[i+4] =0x89;
+    p[i+4] =0x89;// mov    DWORD PTR [rbp-0x4],edi
     p[i+5] =0x7d;
     p[i+6] =0xfc;
-    p[i+7] =0x83;
+    p[i+7] =0x83;// cmp    DWORD PTR [rbp-0x4],0x0
     p[i+8] =0x7d;
     p[i+9] =0xfc;
     p[i+10] =0x00;
-    p[i+11] =0x74;
+    p[i+11] =0x74;// je     e <func+0xe>
     p[i+12] =0x01;
-    p[i+13] =0x90;
-    p[i+14] =0x90;
-    p[i+15] =0x5d;
-    p[i+16] =0xc3;
+    p[i+13] =0x90;// nop
+    p[i+14] =0x90;// nop
+    p[i+15] =0x5d;// pop    rbp
+    p[i+16] =0xc3;// ret
 }
-//TODO: add pht_release where you free everything
+
+
+void pht_release(phtpp_t pht){
+    munmap(pht->memory, FUNC_SIZE*pht->size);
+    bzero(pht, sizeof(struct phtpp));
+    free(pht);
+}
+
 
 phtpp_t pht_prepare(int probe_size){
     phtpp_t pht = (phtpp_t)malloc(sizeof(struct phtpp));
@@ -60,6 +55,7 @@ phtpp_t pht_prepare(int probe_size){
     return pht;
 }
 
+
 typedef void (*fptr1)(int);
 void pht_prime(phtpp_t pht){
     randomize_pht();
@@ -71,6 +67,7 @@ void pht_prime(phtpp_t pht){
         (*((fptr1)p))(0);
     }
 }
+
 
 void pht_probe(phtpp_t pht, uint64_t *results){
     uint64_t start = 0;
