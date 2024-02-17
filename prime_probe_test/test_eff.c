3750ce91ce996f5effef9a19a2e7ed50911cc547
duri12 <eyalgor102@gmail.com>
2024-02-17 14:50:43 +0200

    testing

--- prime_probe_test/test_eff.c
+++ prime_probe_test/test_eff.c
@@ -11,96 +11,30 @@
 volatile uint64_t a;
 
 void MyFunction(char *password) {
-
     if (password[6] != '\0')
         return;
-
     if (password[0] == 'P') {
-        __asm__ __volatile__
-                (
-                "pause;"
-                "movq $0, %%rdx;"
-                "movq $0x10, %%rbx;"
-                "l2%=:"
-                "inc %%rdx;"
-                "cmpq %%rdx, %%rbx;"
-                "jg l2%=;"
-                :
-                :
-                : "rdx", "rbx", "cc", "memory"
-                );
         NOP16();
         NOP16();
         NOP16();
         NOP16();
         if (password[1] == 'A') {
-            __asm__ __volatile__
-                    (
-                    "pause;"
-                    "movq $0, %%rdx;"
-                    "movq $0x10, %%rbx;"
-                    "l3%=:"
-                    "inc %%rdx;"
-                    "cmpq %%rdx, %%rbx;"
-                    "jg l3%=;"
-                    :
-                    :
-                    : "rdx", "rbx", "cc", "memory"
-                    );
             NOP16();
             NOP16();
             NOP16();
             NOP16();
             if (password[2] == 'S') {
-                __asm__ __volatile__
-                        (
-                        "pause;"
-                        "movq $0, %%rdx;"
-                        "movq $0x10, %%rbx;"
-                        "l4%=:"
-                        "inc %%rdx;"
-                        "cmpq %%rdx, %%rbx;"
-                        "jg l4%=;"
-                        :
-                        :
-                        : "rdx", "rbx", "cc", "memory"
-                        );
+
                 NOP16();
                 NOP16();
                 NOP16();
                 NOP16();
                 if (password[3] == 's') {
-                    __asm__ __volatile__
-                            (
-                            "pause;"
-                            "movq $0, %%rdx;"
-                            "movq $0x10, %%rbx;"
-                            "l5%=:"
-                            "inc %%rdx;"
-                            "cmpq %%rdx, %%rbx;"
-                            "jg l5%=;"
-                            :
-                            :
-                            : "rdx", "rbx", "cc", "memory"
-                            );
                     NOP16();
                     NOP16();
                     NOP16();
                     NOP16();
                     if (password[4] == '1') {
-                        __asm__ __volatile__
-                                (
-                                "pause;"
-                                "movq $0, %%rdx;"
-                                "movq $0x10, %%rbx;"
-                                "l6%=:"
-                                "inc %%rdx;"
-                                "cmpq %%rdx, %%rbx;"
-                                "jg l6%=;"
-                                :
-                                :
-                                : "rdx", "rbx", "cc", "memory"
-                                );
                         NOP16();
                         NOP16();
                         NOP16();
@@ -150,11 +84,12 @@ int main(int c, char **v) {
     for (int i  = 0; i <7;i++) {
         printf("tryed %s - got :\n",password[i]);
         for (int j = 0; j <64;j++) {
+            /*
             if(l1Cache[i][j] > 317)
                 l1Cache[i][j] =1;
             else
                 l1Cache[i][j] =0;
-
+            */
              printf("[%d]--[set %d]-> %lu\n",i,j,l1Cache[i][j]);
         }
     }
