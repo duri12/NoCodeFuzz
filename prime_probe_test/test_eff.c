61c0d1bbde0a08c5cab865991326f3f4526a4988
duri12 <eyalgor102@gmail.com>
2024-02-17 14:31:29 +0200

    checking threshold

--- prime_probe_test/test_eff.c
+++ prime_probe_test/test_eff.c
@@ -150,7 +150,7 @@ int main(int c, char **v) {
     for (int i  = 0; i <7;i++) {
         printf("tryed %s - got :\n",password[i]);
         for (int j = 0; j <64;j++) {
-            if(l1Cache[i][j] > 315)
+            if(l1Cache[i][j] > 317)
                 l1Cache[i][j] =1;
             else
                 l1Cache[i][j] =0;
