7da79dc6603c605210d926ccd3f157d9ef1707f8
duri12 <87229324+duri12@users.noreply.github.com>
2024-03-14 18:49:06 +0200

    added the funtion to test

---
+++ myFunction.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <string.h>
+#include <stdint.h>
+//#include "util.h"
+void MyFunction(char *password) __attribute__ ((section(".MyFunction"))); // The section name to use
+__attribute__((optimize("O0")))
+void MyFunction(char *password) {
+    asm("nop");
+    asm("nop");
+
+    if (password[0] == 'P') {
+        asm("nop");
+        if (password[1] == 'A') {
+            asm("nop");
+            if (password[2] == 'S') {
+                asm("nop");
+                if (password[3] == 's') {
+                    asm("nop");
+                    if (password[4] == '1') {
+                        asm("nop");
+                        if (password[5] == '!') {
+                            printf("found the password\n");
+                            printf("the input was %s\n", password);
+                            //sleep(10);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
