8c959f12f9e54f8136647f2ff9fa0dddba1cf79f
duri12 <eyalgor102@gmail.com>
2024-03-18 16:30:22 +0200

    testing bug

--- honggfuzz/side-channels/pht_PP_api.c
+++ honggfuzz/side-channels/pht_PP_api.c
@@ -35,7 +35,7 @@ void pht_release(phtpp_t pht){
 
 phtpp_t pht_prepare(int probe_size){
     phtpp_t pht = (phtpp_t)malloc(sizeof(struct phtpp));
-    pht->memory = mmap((void*)0x30000000, FUNC_SIZE*probe_size, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+    pht->memory = mmap((void*)0x3000000, FUNC_SIZE*probe_size, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
     pht->size = probe_size;
     for (int i = 0; i < probe_size*FUNC_SIZE; i+=FUNC_SIZE)
         memcpy(pht->memory + i, jumpArray, FUNC_SIZE);
