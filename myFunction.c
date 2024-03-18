034febd0ac31c7b680fe4e55478b2cf5e3c52ae0
duri12 <eyalgor102@gmail.com>
2024-03-18 16:06:39 +0200

    testing bug

--- myFunction.c
+++ myFunction.c
@@ -56,10 +56,6 @@ void MyFunction(char *password) {
                 asm("nop");
                 asm("nop");
                 asm("nop");
-                asm("nop");
-                asm("nop");
-                asm("nop");
-                asm("nop");
                 if (password[3] == 's') {
                     asm("nop");
                     asm("nop");
