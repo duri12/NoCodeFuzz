6264016042b076d3ba13fc97ccdd284f6e6a579b
duri12 <eyalgor102@gmail.com>
2024-03-24 20:03:54 +0200

    testing changes

--- honggfuzz/subproc.c
+++ honggfuzz/subproc.c
@@ -848,8 +848,8 @@ static bool subproc_runNoFork(run_t *run)
 
 
     if(password[0] == 'P'){
-        LOG_I("%lu",bpRecordTProbe[0][0][16]);
-        LOG_I("%lu",bpRecordTProbe[1][1][16]);
+        LOG_I("%lu",bpRecordTProbe[0][0][17]);
+        LOG_I("%lu",bpRecordTProbe[1][1][17]);
     }
 
     uint8_t bpResult[PHT_SAMPLE_SIZE*PHT_ARRAY_SIZE] = {0};
