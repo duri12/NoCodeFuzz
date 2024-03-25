3fa386f1953b215c0bbe103cdd68c808b9fb0a0f
duri12 <eyalgor102@gmail.com>
2024-03-25 14:07:00 +0200

    testing changes

--- honggfuzz/subproc.c
+++ honggfuzz/subproc.c
@@ -838,8 +838,8 @@ static bool subproc_runNoFork(run_t *run)
         for (int j = 0; j <PHT_ARRAY_SIZE; ++j) {
             randomize_pht();
             pht_prime(run->scTools.pht[j]);
-            //MyFunction(password);
-            ioctl(f,0,password);
+            MyFunction(password);
+            //ioctl(f,0,password);
             pht_probe(run->scTools.pht[j], &bpRecordTProbe[i][j*PHT_SAMPLE_SIZE]);
         }
 
