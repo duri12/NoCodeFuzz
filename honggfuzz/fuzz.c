a0b369b94466d9904c929956c6a4114a9b5734a9
duri12 <eyalgor102@gmail.com>
2024-03-31 23:41:05 +0300

    testing

--- honggfuzz/fuzz.c
+++ honggfuzz/fuzz.c
@@ -637,7 +637,7 @@ static void* fuzz_threadNew(void* arg) {
      */
 
     srand(1333);
-    run.scTools.pht  = pht_prepare(512,(void*)(uint64_t)0x3000490);
+    run.scTools.pht  = pht_prepare(512,(void*)(uint64_t)0x3000480);
 
     run.scTools.arrs = (int**)malloc(sizeof(int*)*8);
     run.scTools.lengths = (int*)malloc(sizeof(int)*8);
