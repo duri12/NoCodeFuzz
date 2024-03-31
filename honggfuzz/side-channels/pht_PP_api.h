35011e2077e3980f69b08988b28ef597836b5797
duri12 <eyalgor102@gmail.com>
2024-04-01 00:23:58 +0300

    reverted to non random window

--- honggfuzz/side-channels/pht_PP_api.h
+++ honggfuzz/side-channels/pht_PP_api.h
@@ -9,8 +9,8 @@ struct phtpp{
 extern int randomize_pht();
 typedef struct phtpp *phtpp_t;
 phtpp_t pht_prepare(int probe_size,void* where,int offset);
-void pht_prime(phtpp_t pht, int way,int *off , int arrLength);
-void pht_probe(phtpp_t pht, uint64_t *results, int way,int *off , int arrLength);
+void pht_prime(phtpp_t pht);
+void pht_probe(phtpp_t pht, uint64_t *results);
 void pht_release(phtpp_t pht);
 
 #endif //NOCODEFUZZ_PHT_PP_API_H
