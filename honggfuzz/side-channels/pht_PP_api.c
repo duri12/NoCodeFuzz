31be1222f160111878b3fdd4bab10144761c4534
duri12 <eyalgor102@gmail.com>
2024-03-31 22:11:16 +0300

    updated the pht

--- honggfuzz/side-channels/pht_PP_api.c
+++ honggfuzz/side-channels/pht_PP_api.c
@@ -12,80 +12,90 @@
 #define PAGE_SIZE 4096
 #define FUNC_SIZE 16
 
-const uint8_t jumpArray[FUNC_SIZE] = {0x55,0x48,0x89,0xe5,0x89,
-                                       0x7d,0xfc,0x83,0x7d,0xfc,
-                                       0x00,0x74,0x01,0x90,0x5d,
-                                       0xc3};
+//const uint8_t jumpArray[FUNC_SIZE] = {0x83, 0xff, 0x00, 0x90,0x74,  0x01, 0x90, 0xc3};
+const uint8_t jumpArray[FUNC_SIZE] = {0x40, 0x31, 0xd2, 0x89, 0xf8, 0xff, 0xc7, 0xf7, 0xff, 0x85, 0xd2, 0x74, 0x02,
+                                      0x48, 0x48, 0xc3};
+//const uint8_t jumpArray[FUNC_SIZE] = {0x55,0x48,0x89,0xe5,0x89,
+//                                      0x7d,0xfc,0x83,0x7d,0xfc,
+//                                      0x00,0x74,0x01,0x90,0x5d,
+//                                      0xc3};
+//const uint8_t jumpArray[FUNC_SIZE] = {0x90,0x90,0x90,0x90,0x90,0x90,0x90,0x90,0x83,0xff,0x00,0x90,0x75,0x01,0x90,0xc3};
+
+//{0x31,0xd2,0x89,0xf8,0xbf,0x01,0x00,0x00,0x00,0xf7,0xff,0x85
+//,0xc0,0x74,0x00,0xc3};
+
 /*
-0:  55                      push   ebp
-1:  48                      dec    eax
-2:  89 e5                   mov    ebp,esp
-4:  89 7d fc                mov    DWORD PTR [ebp-0x4],edi
-7:  83 7d fc 00             cmp    DWORD PTR [ebp-0x4],0x0
-b:  75 01                   jne    0xe
-d:  90                      nop
-e:  5d                      pop    ebp
+0:  40                      inc    eax
+1:  31 d2                   xor    edx,edx
+3:  89 f8                   mov    eax,edi
+5:  ff c7                   inc    edi
+7:  f7 ff                   idiv   edi
+9:  85 d2                   test   edx,edx
+b:  74 02                   je     0xf
+d:  48                      dec    eax
+e:  48                      dec    eax
 f:  c3                      ret
  */
-
-void pht_release(phtpp_t pht){
-    munmap(pht->memory, FUNC_SIZE*pht->size);
+void pht_release(phtpp_t pht) {
+    munmap(pht->memory, FUNC_SIZE * pht->size);
     free(pht);
 }
 
-phtpp_t pht_prepare(int probe_size,void* where,int offset){ //0x3000000
-    phtpp_t pht = (phtpp_t)malloc(sizeof(struct phtpp));
-    pht->memory = mmap(where, FUNC_SIZE*probe_size+offset, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+phtpp_t pht_prepare(int probe_size, void *where, int offset) { //0x3000000
+    phtpp_t pht = (phtpp_t) malloc(sizeof(struct phtpp));
+    pht->memory = mmap(where, FUNC_SIZE * probe_size + offset, PROT_READ | PROT_WRITE | PROT_EXEC,
+                       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     pht->size = probe_size;
     pht->memory += offset;
-    printf("%p\n",pht->memory);
-    for (int i = 0; i < probe_size*FUNC_SIZE; i+=FUNC_SIZE)
+    for (int i = 0; i < probe_size * FUNC_SIZE; i += FUNC_SIZE)
         memcpy(pht->memory + i, jumpArray, FUNC_SIZE);
     return pht;
 }
 
-typedef void (*fptr1)(int);
+//typedef void (*fptr1)(int);
+typedef void (*fptr1)();
 
-void pht_prime(phtpp_t pht){
+void pht_prime(phtpp_t pht) {
     void *p = pht->memory;
     int i = pht->size;
 
-start_label:
-    (*((fptr1)p))(0);
-    (*((fptr1)p))(0);
-    (*((fptr1)p))(0);
-    (*((fptr1)p))(0);
-    
+    start_label:
+    (*((fptr1) p))(0);
+    (*((fptr1) p))(0);
+    (*((fptr1) p))(0);
+    (*((fptr1) p))(0);
+    (*((fptr1) p))(0);
+
     p += FUNC_SIZE;
-    
+
     CONDITIONAL_JUMP_TO_LABEL(i, start_label);
-    __asm__ volatile(".global pht_prepare_end\n\t"
-                    "pht_prepare_end:");
-}
+        __asm__ volatile(".global pht_prepare_end\n\t"
+                         "pht_prepare_end:");
+    }
 
 static FORCE_INLINE void cpuid_serializing(void) {
     __asm__ volatile(
-        "xor %%eax, %%eax\n\t"
-        "cpuid" ::: "eax", "ebx", "ecx", "edx"
-    );
+            "xor %%eax, %%eax\n\t"
+            "cpuid":: : "eax", "ebx", "ecx", "edx"
+            );
 }
 
-void* ignore_me;
+void *ignore_me;
 
 // Disable optimizations
 __attribute__((optimize("O0")))
-void pht_probe(phtpp_t pht, uint64_t *results){
+void pht_probe(phtpp_t pht, uint64_t *results) {
     register void *p = pht->memory;
     register int i = pht->size;
     register uint64_t *p_result = results;
     register uint64_t start, end;
     uint32_t ecx;
 
-start_label:
-    (*((fptr1)p))(1);
+    start_label:
+    (*((fptr1) p))(1);
     start = _rdtscp(&ecx);
     _mm_mfence();
-    (*((fptr1)p))(1);
+    (*((fptr1) p))(1);
     end = _rdtscp(&ecx);
     cpuid_serializing();
     *p_result = end - start;
@@ -93,6 +103,6 @@ start_label:
     p += FUNC_SIZE;
 
     CONDITIONAL_JUMP_TO_LABEL(i, start_label);
-    __asm__ volatile(".global pht_probe_end\n\t"
-                    "pht_probe_end:");
-}
+        __asm__ volatile(".global pht_probe_end\n\t"
+                         "pht_probe_end:");
+    }
