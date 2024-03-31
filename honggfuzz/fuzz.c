cb86272ecf604cebb3af09e6de9a405f549be7f1
duri12 <eyalgor102@gmail.com>
2024-04-01 01:26:24 +0300

    fixed some staff

--- honggfuzz/fuzz.c
+++ honggfuzz/fuzz.c
@@ -635,7 +635,7 @@ static void* fuzz_threadNew(void* arg) {
      */
 
     for (int i = 0; i <NUM_OF_PHT; ++i) {
-        run.scTools.pht[i]  = pht_prepare(NUM_OF_PHT,(void*)(int)(0x3000000+0x1200000*i),2*i);
+        run.scTools.pht[i]  = pht_prepare(NUM_OF_PHT,(void*)(uint64_t)(0x3000000+0x120000000*i),2*i);
     }
     //TODO: create a constant for probe size
 
